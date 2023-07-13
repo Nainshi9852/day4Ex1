3e12053d03df4078702a5aad297ac083afffe8a9
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-07-13 09:45:50 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter marks in the pre-exam:");
+            int preMarks = Convert.ToInt32(Console.ReadLine());
+
+            if (preMarks > 60)
+            {
+                Console.WriteLine("Enter marks in the final exam:");
+                int finalMarks = Convert.ToInt32(Console.ReadLine());
+
+                if (finalMarks > 55)
+                {
+                    Console.WriteLine("Result: Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Result: Fail in Final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Result: Fail in Pre");
+            }
+            Console.ReadKey();
+        }
+    }
+}
